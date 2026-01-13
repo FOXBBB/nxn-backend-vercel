4046e638f84c4370b5955e3f3950cefa40ed5270
FOXBBB <ibraqimovnatik@gmail.com>
2026-01-13 21:37:49 +0300

    Refactor handler to use global users map

--- api/tap.js
+++ api/tap.js
@@ -1,8 +1,4 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-const users = new Map<number, any>();
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -13,15 +9,17 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
     return res.status(400).json({ error: "telegramId required" });
   }
 
-  if (!users.has(telegramId)) {
-    users.set(telegramId, {
+  global.users = global.users || new Map();
+
+  if (!global.users.has(telegramId)) {
+    global.users.set(telegramId, {
       telegramId,
       balance: 0,
       energy: 100
     });
   }
 
-  const user = users.get(telegramId);
+  const user = global.users.get(telegramId);
 
   if (user.energy > 0) {
     user.balance += 1;
